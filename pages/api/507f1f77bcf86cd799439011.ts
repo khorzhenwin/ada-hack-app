d068a94f90572d6bffc41051d4985d5984bdde33
Boey01 <boeyshuwei@gmail.com>
2023-10-08 23:44:45 +0800

    make the receive test works and return exact body

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -5,14 +5,21 @@ export default async function handler(req, res) {
     return;
   }
 
-  // check bearer token
+  // Check bearer token
   const bearerToken = req.headers.authorization;
   if (!bearerToken || bearerToken !== "Ada@2023") {
     res.status(401).json({ message: "Unauthorized" });
     return;
   }
 
-  console.log(req);
+  try {
+    // Parse the JSON data from the request body
+    const reqBodyString = JSON.stringify(req.body);
+    const requestData = JSON.parse(reqBodyString);
 
-  res.status(200).json(req);
+    // Respond with the parsed JSON data
+    res.status(200).json(requestData);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid JSON data in the request body" });
+  }
 }
