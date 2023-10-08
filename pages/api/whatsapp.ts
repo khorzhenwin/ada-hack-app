8c3a0f505992a0943437191f92f18828ae9bb930
Boey01 <boeyshuwei@gmail.com>
2023-10-09 00:23:02 +0800

    fix req body error

--- pages/api/whatsapp.ts
+++ pages/api/whatsapp.ts
@@ -1,21 +1,39 @@
-
-  // this is a POST method
- export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
+// POST Method
+export default async function handler(req, res) {
+  //   if (req.method !== "POST") {
+  //     res.status(405).json({ message: "Method not allowed" });
+  //     return;
+  //   }
+  // Check bearer token
+  const bearerToken = req.headers.authorization;
+  if (!bearerToken || bearerToken !== "Ada@2023") {
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
+  
+  try {
+    // Parse the JSON data from the request body
+    const reqBodyString = JSON.stringify(req.body);
+    const requestData = JSON.parse(reqBodyString);
 
-  //the request body
-    req.body.platform = 'WA';
-    req.body.type = 'text';
-    req.body.from = '60136959014'
-    
-  // Construct the request
-     sendWAmessage(req);
+    // Respond with the parsed JSON data
+    res.status(200).json(requestData);
+
+    sendWAmessage(requestData);
+
+  } catch (error) {
+    res.status(400).json({ message: "Invalid JSON data in the request body" });
+  }
 }
 
+
 async function sendWAmessage(req) {
+  req.body.platform = 'WA';
+  req.body.type = 'text';
+  req.body.from = '60136959014'
+  req.body.to = "60175228785"
+  req.body.text = req;
+
   const waAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IjY2ZDYzNDY0LTJhNjktNDg1MS05MjI1LTZjY2U3MDY2YTE2NiIsImNvdW50cnlDb2RlIjoiIiwiZW1haWwiOiJUZWFtMjBAYWRhLWFzaWEuY29tIiwiZXhwIjoyMzI3ODk1ODcyLCJpYXQiOjE2OTY3NDM4NzIsIm5hbWUiOiJBRE1JTiIsInJvbGVDb2RlIjoiT1dORVIiLCJyb2xlSWQiOiJPV05FUiIsInNpZCI6ImFwaWtleSIsInN0eXBlIjoidXNlciIsInVpZCI6ImM4OWJmNGMwLWIwMzktNGEyZC1hYjEzLTBhNDNiMTgwNGRjYyJ9.K3-8lHBq1_7VfD5jXbzW8s6sUma1HKIYRYKpQT-HBVc";
 
   const endpoint = "https://bizmsgapi.ada-asia.com/prod/message"; 
