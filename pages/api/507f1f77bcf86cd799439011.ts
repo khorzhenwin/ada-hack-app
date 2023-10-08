a026a7e3f3cd99598f54104e2adfc5dd2a5287ab
Boey01 <boeyshuwei@gmail.com>
2023-10-09 01:17:58 +0800

    adding return

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   }
 
  await sendWAmessage(text);
-  res.status(200).json(text);
+  return res.status(200).json(text);
 }
 
 async function sendWAmessage(text) {
