07f05fa8faeb169bec2935afb808dd187a2e88b6
Boey01 <boeyshuwei@gmail.com>
2023-10-09 01:19:58 +0800

    minor adjustment on the test api

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
     return;
   }
 
- await sendWAmessage(text);
-  return res.status(200).json(text);
+  await sendWAmessage(text);
+  return res.status(200).json({ message: "success" });
 }
 
 async function sendWAmessage(text) {
