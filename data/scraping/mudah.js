d7cafd614b6ad4aca0bcb7dca85d90cb727de78b
Chong Rui Jun <chongruijun@gmail.com>
2023-10-08 18:39:47 +0800

    mudah.my done

--- data/scraping/mudah.js
+++ data/scraping/mudah.js
@@ -7,12 +7,12 @@ const cheerio = require("cheerio");
 /**
  * @param {string} product - product to be searched
  * @param {number} pageNo - get the data of the top (x) to top (x + 2) products, x = pageNo
- * @returns json object containing the products
+ * @returns {Array<JSON>} json object containing the products
  */
 export async function getMudahMyProducts(product = "", pageNo = 1) {
   try {
     console.log(
-      "Starting scraping process from mudah.my for '+ " + product + "' ......"
+      `Starting scraping process from mudah.my for '${product}' ......`
     );
 
     const search_url =
@@ -29,10 +29,27 @@ export async function getMudahMyProducts(product = "", pageNo = 1) {
       },
     });
 
-    var product_json = {};
+    var product_array = [];
     const $ = cheerio.load(response_request.data);
 
     // mudah.my shuffles between multiple html class names on each reload randomly
+
+    // The product image url
+    var image_html_class = "";
+    if ($(".mw1291").length != 0) image_html_class = ".mw1291";
+    else if ($(".mw357").length != 0) image_html_class = ".mw357";
+    else {
+      // checking if there are any products found
+      console.log(`==> No products of '${product}' found on mudah.my`);
+      return [];
+    }
+    // console.log(image_html_class + " : " + $(image_html_class).length);
+    $(image_html_class).each((i, e) => {
+      product_array.push({
+        image: "",
+        // image: $(e).attr().src.replace(/(\s+)/g, " ").trim(),
+      });
+    });
 
     // The product names
     var name_html_class = "";
@@ -44,15 +61,9 @@ export async function getMudahMyProducts(product = "", pageNo = 1) {
       name_html_class = ".sc-eInJlc.iCLkHj";
     else if ($(".sc-jwKygS.BZRPK").length != 0)
       name_html_class = ".sc-jwKygS.BZRPK";
-    else {
-      // checking if there are any products found
-      console.log(`==> No products of '${product}' found on mudah.my`);
-      return {};
-    }
     // console.log(name_html_class + " : " + $(name_html_class).length);
     $(name_html_class).each((i, e) => {
-      product_json[(i + 1).toString()] = {};
-      product_json[(i + 1).toString()]["name"] = $(e)
+      product_array[i]["name"] = $(e)
         .attr()
         .title.replace(/(\s+)/g, " ")
         .trim();
@@ -67,11 +78,40 @@ export async function getMudahMyProducts(product = "", pageNo = 1) {
     else price_html_class = ".sc-hzDEsm.ftGocf";
     // console.log(price_html_class + " : " + $(price_html_class).length);
     $(price_html_class).each((i, e) => {
-      product_json[(i + 1).toString()]["price"] = $(e)
+      product_array[i]["price"] = $(e)
         .text()
         .replace(/(\s+)/g, " ")
         .replace("RM ", "")
         .trim();
+      product_array[i]["rating"] = "";
+      product_array[i]["seller_name"] = "";
+    });
+
+    // The product seller location
+    var location_html_class = "";
+    if ($(".sc-dNLxif.eWZRuS").length != 0)
+      location_html_class = ".sc-dNLxif.eWZRuS";
+    else if ($(".sc-TFwJa.eHevBI").length != 0)
+      location_html_class = ".sc-TFwJa.eHevBI";
+    else location_html_class = ".sc-kxynE.jyeGVA";
+    // console.log(location_html_class + " : " + $(location_html_class).length);
+    $(location_html_class).each((i, e) => {
+      product_array[i]["seller_location"] = $(e)
+        .text()
+        .replace(/(\s+)/g, " ")
+        .trim();
+    });
+
+    // The product page url
+    var productURL_html_class = "";
+    if ($(".sc-giadOv.dOjdcz").length != 0)
+      productURL_html_class = ".sc-giadOv.dOjdcz";
+    else if ($(".sc-cIShpX.jvoCyo").length != 0)
+      productURL_html_class = ".sc-cIShpX.jvoCyo";
+    else productURL_html_class = ".sc-etwtAo.jTnCIh";
+    // console.log(productURL_html_class + " : " + $(productURL_html_class).length);
+    $(productURL_html_class).each((i, e) => {
+      product_array[i]["url"] = $(e).attr().href.trim();
     });
 
     // The product condition
@@ -85,7 +125,7 @@ export async function getMudahMyProducts(product = "", pageNo = 1) {
     var temp = 0;
     $(condition_html_class).each((_, e) => {
       if ($(e).attr().title == "Condition") {
-        product_json[(temp + 1).toString()]["condition"] = $(e)
+        product_array[temp]["condition"] = $(e)
           .children("div")
           .text()
           .replace(/(\s+)/g, " ")
@@ -96,11 +136,24 @@ export async function getMudahMyProducts(product = "", pageNo = 1) {
 
     // console.log(product_json);
     console.log(`Done scraping mudah.my for '${product}'`);
-    console.log("==> Products found: ", Object.keys(product_json).length);
+    console.log("==> Products found: ", Object.keys(product_array).length);
 
-    return product_json;
+    if (product_array.length < pageNo * 3) {
+      if (product_array.length < pageNo * 3 - 2) {
+        console.log("No more products available for the required page number");
+        return [];
+      } else {
+        return product_array.slice(pageNo * 3 - 3);
+      }
+    } else {
+      return product_array.slice(pageNo * 3 - 3, pageNo * 3);
+    }
   } catch (error) {
     console.log(error);
-    return {};
+    return [];
   }
 }
+
+// getMudahMyProducts("monitor stand", 2).then((arr) => {
+//   console.log(arr);
+// });
