6ce8d673aeaf7f01bb90fd5ccb4028a0465df586
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-10-09 00:34:53 +0800

    testing

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -4,14 +4,14 @@ export default async function handler(req, res) {
   //     res.status(405).json({ message: "Method not allowed" });
   //     return;
   //   }
-  
+
   // Check bearer token
   const bearerToken = req.headers.authorization;
   if (!bearerToken || bearerToken !== "Ada@2023") {
     res.status(401).json({ message: "Unauthorized" });
     return;
   }
-  
+
   try {
     // Parse the JSON data from the request body
     const reqBodyString = JSON.stringify(req.body);
@@ -27,16 +27,17 @@ export default async function handler(req, res) {
 }
 
 async function sendWAmessage(req) {
-  req.body = {}
-  req.body.platform = 'WA';
-  req.body.type = 'text';
-  req.body.from = '60136959014'
-  req.body.to = "60175228785"
-  req.body.text = req;
+  req.body = {};
+  req.body["platform"] = "WA";
+  req.body["type"] = "text";
+  req.body["from"] = "60136959014";
+  req.body["to"] = "60175228785";
+  req.body["text"] = req;
 
-  const waAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IjY2ZDYzNDY0LTJhNjktNDg1MS05MjI1LTZjY2U3MDY2YTE2NiIsImNvdW50cnlDb2RlIjoiIiwiZW1haWwiOiJUZWFtMjBAYWRhLWFzaWEuY29tIiwiZXhwIjoyMzI3ODk1ODcyLCJpYXQiOjE2OTY3NDM4NzIsIm5hbWUiOiJBRE1JTiIsInJvbGVDb2RlIjoiT1dORVIiLCJyb2xlSWQiOiJPV05FUiIsInNpZCI6ImFwaWtleSIsInN0eXBlIjoidXNlciIsInVpZCI6ImM4OWJmNGMwLWIwMzktNGEyZC1hYjEzLTBhNDNiMTgwNGRjYyJ9.K3-8lHBq1_7VfD5jXbzW8s6sUma1HKIYRYKpQT-HBVc";
+  const waAPIKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IjY2ZDYzNDY0LTJhNjktNDg1MS05MjI1LTZjY2U3MDY2YTE2NiIsImNvdW50cnlDb2RlIjoiIiwiZW1haWwiOiJUZWFtMjBAYWRhLWFzaWEuY29tIiwiZXhwIjoyMzI3ODk1ODcyLCJpYXQiOjE2OTY3NDM4NzIsIm5hbWUiOiJBRE1JTiIsInJvbGVDb2RlIjoiT1dORVIiLCJyb2xlSWQiOiJPV05FUiIsInNpZCI6ImFwaWtleSIsInN0eXBlIjoidXNlciIsInVpZCI6ImM4OWJmNGMwLWIwMzktNGEyZC1hYjEzLTBhNDNiMTgwNGRjYyJ9.K3-8lHBq1_7VfD5jXbzW8s6sUma1HKIYRYKpQT-HBVc";
 
-  const endpoint = "https://bizmsgapi.ada-asia.com/prod/message"; 
+  const endpoint = "https://bizmsgapi.ada-asia.com/prod/message";
 
   const header = {
     "Content-Type": "application/json", // Set Content-Type to application/json
