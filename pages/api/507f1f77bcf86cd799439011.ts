c9470933d7211b1a7fb81f4cfd96351d1830442b
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-10-08 23:26:45 +0800

    comment out webhook endpoint

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -1,16 +1,16 @@
 // POST Method
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-    return;
-  }
+  //   if (req.method !== "POST") {
+  //     res.status(405).json({ message: "Method not allowed" });
+  //     return;
+  //   }
 
-  // check bearer token
-  const bearerToken = req.headers.authorization;
-  if (!bearerToken || bearerToken !== "Ada@2023") {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
-  }
+  //   // check bearer token
+  //   const bearerToken = req.headers.authorization;
+  //   if (!bearerToken || bearerToken !== "Ada@2023") {
+  //     res.status(401).json({ message: "Unauthorized" });
+  //     return;
+  //   }
 
   console.log(req);
 
