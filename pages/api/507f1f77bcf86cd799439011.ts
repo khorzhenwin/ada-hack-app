23269e0d2454c4c3ca2d61441836cc9a15bbde94
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-10-09 00:40:10 +0800

    test reformat

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
   //     return;
   //   }
 
+  const { text } = req.body;
+
   // Check bearer token
   const bearerToken = req.headers.authorization;
   if (!bearerToken || bearerToken !== "Ada@2023") {
@@ -12,27 +14,17 @@ export default async function handler(req, res) {
     return;
   }
 
-  try {
-    // Parse the JSON data from the request body
-    const reqBodyString = JSON.stringify(req.body);
-    const requestData = JSON.parse(reqBodyString);
-
-    // Respond with the parsed JSON data
-    sendWAmessage(requestData);
-
-    res.status(200).json(requestData);
-  } catch (error) {
-    res.status(400).json({ message: "Invalid JSON data in the request body" });
-  }
+  sendWAmessage(text);
+  res.status(200).json(text);
 }
 
-async function sendWAmessage(req) {
-  req.body = {};
-  req.body["platform"] = "WA";
-  req.body["type"] = "text";
-  req.body["from"] = "60136959014";
-  req.body["to"] = "60175228785";
-  req.body["text"] = req;
+async function sendWAmessage(text) {
+  const req = {};
+  req["platform"] = "WA";
+  req["type"] = "text";
+  req["from"] = "60136959014";
+  req["to"] = "60175228785";
+  req["text"] = text;
 
   const waAPIKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IjY2ZDYzNDY0LTJhNjktNDg1MS05MjI1LTZjY2U3MDY2YTE2NiIsImNvdW50cnlDb2RlIjoiIiwiZW1haWwiOiJUZWFtMjBAYWRhLWFzaWEuY29tIiwiZXhwIjoyMzI3ODk1ODcyLCJpYXQiOjE2OTY3NDM4NzIsIm5hbWUiOiJBRE1JTiIsInJvbGVDb2RlIjoiT1dORVIiLCJyb2xlSWQiOiJPV05FUiIsInNpZCI6ImFwaWtleSIsInN0eXBlIjoidXNlciIsInVpZCI6ImM4OWJmNGMwLWIwMzktNGEyZC1hYjEzLTBhNDNiMTgwNGRjYyJ9.K3-8lHBq1_7VfD5jXbzW8s6sUma1HKIYRYKpQT-HBVc";
@@ -47,7 +39,7 @@ async function sendWAmessage(req) {
   const res = await fetch(endpoint, {
     method: "POST",
     headers: header,
-    body: JSON.stringify(req.body),
+    body: JSON.stringify(req),
   });
 
   if (res.status === 200 || res.status === 201) {
