870120430fc4b834042527d1dce1f1de7e68c44d
Boey01 <boeyshuwei@gmail.com>
2023-10-09 00:14:33 +0800

    Instantly send to boey's whatsapp after gettin msg

--- pages/api/507f1f77bcf86cd799439011.ts
+++ pages/api/507f1f77bcf86cd799439011.ts
@@ -20,7 +20,41 @@ export default async function handler(req, res) {
 
     // Respond with the parsed JSON data
     res.status(200).json(requestData);
+
+    sendWAmessage(requestData);
+
   } catch (error) {
     res.status(400).json({ message: "Invalid JSON data in the request body" });
+  }
+}
+
+
+async function sendWAmessage(req) {
+  let  responseBody;
+  responseBody.platform = 'WA';
+  responseBody.type = 'text';
+  responseBody.from = '60136959014'
+  responseBody.to = "60175228785"
+  responseBody.text = req;
+
+  const waAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6IjY2ZDYzNDY0LTJhNjktNDg1MS05MjI1LTZjY2U3MDY2YTE2NiIsImNvdW50cnlDb2RlIjoiIiwiZW1haWwiOiJUZWFtMjBAYWRhLWFzaWEuY29tIiwiZXhwIjoyMzI3ODk1ODcyLCJpYXQiOjE2OTY3NDM4NzIsIm5hbWUiOiJBRE1JTiIsInJvbGVDb2RlIjoiT1dORVIiLCJyb2xlSWQiOiJPV05FUiIsInNpZCI6ImFwaWtleSIsInN0eXBlIjoidXNlciIsInVpZCI6ImM4OWJmNGMwLWIwMzktNGEyZC1hYjEzLTBhNDNiMTgwNGRjYyJ9.K3-8lHBq1_7VfD5jXbzW8s6sUma1HKIYRYKpQT-HBVc";
+
+  const endpoint = "https://bizmsgapi.ada-asia.com/prod/message"; 
+
+  const header = {
+    "Content-Type": "application/json", // Set Content-Type to application/json
+    Authorization: `Bearer ${waAPIKey}`,
+  };
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    headers: header,
+    body: JSON.stringify(req.body),
+  });
+
+  if (res.status === 200 || res.status === 201) {
+    return res.json();
+  } else {
+    return null;
   }
 }
